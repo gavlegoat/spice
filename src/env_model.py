148b8b6d37c10f4322e3bdfc6ef5479a19a5764d
Greg Anderson <ganderso@cs.utexas.edu>
2022-11-23 12:16:26 -0600

    Fixed bug

--- src/env_model.py
+++ src/env_model.py
@@ -38,7 +38,8 @@ class ResidualEnvModel(torch.nn.Module):
         layers = []
         for i in range(1, len(arch)):
             layers.append(torch.nn.Linear(arch[i-1], arch[i]))
-            layers.append(torch.nn.Tanh())
+            if i < len(arch) - 1:
+                layers.append(torch.nn.Tanh())
         self.net = torch.nn.Sequential(*layers)
         self.inp_means = torch.nn.Parameter(
             torch.tensor(input_means, dtype=torch.float32),
